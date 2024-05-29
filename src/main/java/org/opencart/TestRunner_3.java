c3f1e6d32f325dbb639053f284cf274580e94d32
Adrian Cirlig <adrian07cirlig@gmail.com>
2024-05-29 18:15:40 +0300

    Completarea claselor si a test runner 3

--- src/main/java/org/opencart/TestRunner_3.java
+++ src/main/java/org/opencart/TestRunner_3.java
@@ -1,2 +1,57 @@
-package org.opencart;public class TestRunner_3 {
+package org.opencart;
+
+import org.opencart.managers.DriverManager;
+import org.opencart.managers.FakeDataManager;
+import org.opencart.pageobjects.AccountPage;
+import org.opencart.pageobjects.HomePage;
+import org.opencart.pageobjects.LoginPage;
+import org.opencart.pageobjects.RegisterPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TestRunner_3 {
+    public static void main (String[] agrs) throws InterruptedException{
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://tekwillacademy-opencart.online/");
+
+        driver.manage().window().maximize();
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegisterPage();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickOnContinueButton();
+        String randomEmail = FakeDataManager.getRandomEmail();
+        Thread.sleep(2000);
+
+        registerPage.completeTheRegisterForm("Adrian", "Cirlig", randomEmail,"password1234!");
+        Thread.sleep(2000);
+
+        registerPage.enablePrivacyToggle();
+        Thread.sleep(2000);
+
+        registerPage.clickOnContinueButton();
+        Thread.sleep(2000);
+
+        WebElement clickContinue = driver.findElement(By.xpath("//a[normalize-space()='Continue']"));
+        clickContinue.click();
+        Thread.sleep(2000);
+
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.navigateToDropDown();
+        accountPage.clickOnLogOutButton();
+        Thread.sleep(2000);
+
+        homePage.navigateToLoginPage();
+        Thread.sleep(2000);
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.completeTheLoginForm(randomEmail, "password1234!");
+        loginPage.clickOnContinueButton();
+        Thread.sleep(2000);
+
+//        driver.quit();
+//        System.out.println("The test is finished and the driver is closed");
+    }
 }
--- src/main/java/org/opencart/pageobjects/AccountPage.java
+++ src/main/java/org/opencart/pageobjects/AccountPage.java
@@ -1,4 +1,21 @@
 package org.opencart.pageobjects;
 
-public class AccountPage {
+import org.opencart.managers.ScrollManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountPage extends Page{
+    public AccountPage(WebDriver driver){
+        super(driver);
+    }
+    @FindBy(xpath = "//a[@class='dropdown-item'][normalize-space()='Logout']")
+    private WebElement logOutBtn;
+    public void clickOnLogOutButton()
+    {
+        ScrollManager.scrollToElement(logOutBtn);
+        logOutBtn.click();
+        System.out.println("The logOut btn was clicked");
+    }
+
 }
